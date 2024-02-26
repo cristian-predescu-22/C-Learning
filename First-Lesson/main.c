51ecd804d81eab517078d3d2b92e6a44d8c11fd4
Cristian Predescu <cristipredescu21@yahoo.com>
2024-02-26 13:05:34 +0000

    Finished the first lesson about prints and comments

---
+++ First-Lesson/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  //COMMENT LETS GOOO
+  /* 
+  OOOO  MILTI LINE
+  nice :)
+  */
+  printf(":)\n");
+  printf("nice\n");
+  printf("1\t2\t3\n");
+  printf("\"I like my pizza\"\n");
+  return 0; 
+}
