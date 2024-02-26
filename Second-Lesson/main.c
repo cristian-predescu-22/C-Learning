33a8f0048278ac277cf84f875c00481162c0121b
Cristian Predescu <cristipredescu21@yahoo.com>
2024-02-26 13:22:24 +0000

    Added second lesson and a Templates folder for all future lessons

---
+++ Second-Lesson/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+  
+  int x; // decleration
+  x = 12; // initilazation
+  int y = 42; // Both D+I
+  
+  int age = 23; //intiger
+  float gpa = 2.65; //floting point
+  char grade = 'C'; //single charecter
+  char name[] = "Cristian"; //This is an array of charecters
+  
+  printf("Hello %s\n", name);
+  printf("Hello user you are %d years old\n", age);
+  return 0;
+}
